41d15a364450bdb26242481445625026089fecd0
kbu715 <kbu715@gmail.com>
2021-08-22 20:57:39 +0900

    08.22. study

--- 자바스크립트기본/script.js
+++ 자바스크립트기본/script.js
@@ -1,1 +1,13 @@
-alert("자바스크립트!!!");
+"use strict";
+// 이 코드는 모던한 방식으로 실행됩니다.
+
+// alert("자바스크립트!!!");
+
+let admin;
+let name = "John";
+
+admin = name;
+console.log(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName;
