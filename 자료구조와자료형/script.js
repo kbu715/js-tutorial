a04f705db84a8a393231153e3e181ce8b3b2f5f2
kbu715 <kbu715@gmail.com>
2021-11-06 15:43:37 +0900

    📝 Study: 숫자형

--- 자료구조와자료형/script.js
+++ 자료구조와자료형/script.js
@@ -1,10 +1,10 @@
 "use strict"
 
-let str = "Hello";
+// let str = "Hello";
 
-str.test = 5; // (*)
+// str.test = 5; // (*)
 
-console.log(str.test);
+// console.log(str.test);
 
 /*
 엄격 모드인지 아닌지에 따라 결과가 나뉩니다.
@@ -21,3 +21,76 @@ str의 프로퍼티에 접근하려 하면 "래퍼 객체"가 만들어집니다
 
 원시값은 추가 데이터를 저장할 수 없습니다.
 */
+
+
+// 수를 입력받아 덧셈하기
+
+// const a = +prompt('The first number?', "");
+// const b = +prompt('The first number?', "");
+
+// alert(a+b);
+
+
+// 6.35.toFixed(1) == 6.3인 이뉴는 무엇일까요?
+
+// 이 문서에 따르면 Math.round와 toFixed는 둘 다 가장 가까운 어림수를 구해줍니다. 
+// 0..4는 버림하고, 5..9는 올림합니다.
+
+// 10진법으로 나타낸 소수 6.35는 내부적으로는 2진법 무한소수입니다. 
+// 따라서 이 경우에도 어김없이 정밀도 손실이 발생합니다.
+
+// 아래 코드를 살펴봅시다.
+
+alert( 6.35.toFixed(20) ); // 6.34999999999999964473
+// 정밀도 손실은 수를 증가시킬 수도, 감소시킬 수도 있습니다. 
+// 위 예시에서는 수가 아주 약간 작아졌습니다. 따라서 반올림하면 버림이 일어납니다.
+
+// 1.35의 경우에는 어떨까요?
+
+alert( 1.35.toFixed(20) ); // 1.35000000000000008882
+// 이번에는 정밀도 손실로 수가 약간 증가했습니다. 따라서 반올림하면 올림이 일어납니다.
+
+// 6.35를 제대로 반올림하려면 어떻게 해야 할까요?
+
+// 반올림하기 전에 이 수를 정수에 가깝게 만들어야 합니다.
+
+alert( (6.35 * 10).toFixed(20) ); // 63.50000000000000000000
+// 63.5 에서는 정밀도 손실이 전혀 발생하지 않습니다. 
+// 소수 부분인 0.5가 정확히 1/2이기 때문입니다. 
+// 2진법 체계에서 2의 거듭제곱으로 나눈 값은 정확하게 저장되기 때문에 제대로 반올림할 수 있습니다.
+
+alert( Math.round(6.35 * 10) / 10); // 6.35 -> 63.5 -> 64(반올림됨) -> 6.4
+
+
+
+
+// 숫자를 입력할 때까지 반복하기
+
+/*
+function readNumber() {
+  let num;
+
+  do {
+    num = prompt("Enter a number please?", 0);
+  } while ( !isFinite(num) );
+
+  if (num === null || num === '') return null;
+
+  return +num;
+}
+
+alert(`Read: ${readNumber()}`);
+
+*/
+
+// A random number from min to max
+
+function random(min, max) {
+  return Math.random() * (max-min) + min
+}
+
+// A random integer from min to max
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max-min) + min)
+}
