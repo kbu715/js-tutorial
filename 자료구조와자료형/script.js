686fda66f3918fccdd07267ab242673cdb7f109e
kbu715 <kbu715@gmail.com>
2021-11-16 17:01:40 +0900

    📝 Study: 배열과 메서드 - part 2

--- 자료구조와자료형/script.js
+++ 자료구조와자료형/script.js
@@ -269,3 +269,246 @@ function sumInput() {
 }
 
 alert( sumInput() );
+
+
+
+// border-left-width를 borderLeftWidth로 변경하기
+
+
+
+
+function camelize(str) {
+  return str.split('-')
+          .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+          .join('');
+}
+
+console.log(camelize("background-color"));
+console.log(camelize("list-style-image"));
+console.log(camelize("-webkit-transition"));
+
+
+
+
+
+
+
+
+
+
+// 특정 범위에 속하는 요소 찾기
+
+function filterRangeInPlace(array, a, b) {
+  arr = array.filter((item) => (item >= a && item <= b));
+}
+
+let arr = [5, 3, 8, 1];
+
+filterRangeInPlace(arr, 1, 4); // 1과 4 사이에 있지 않은 요소는 모두 제거함
+
+alert( arr ); // [3, 1]
+
+
+
+
+
+
+
+
+
+// 내림차순 정렬하기
+
+// let arr = [5, 2, 1, -10, 8];
+
+// // 요소를 내림차순으로 정렬해주는 코드를 여기에 작성해보세요.
+
+// arr.sort((a, b) => b-a);
+
+// alert( arr ); // 8, 5, 2, 1, -10
+
+
+
+// 배열 복사본을 정렬하기
+
+
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+function copySorted(array) {
+  return array.slice().sort();
+}
+
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (no changes)
+
+
+
+
+
+// 확장 가능한 계산기
+
+function Calculator() {
+
+  this.methods = {
+    "-": (a, b) => a - b,
+    "+": (a, b) => a + b
+  };
+
+  this.calculate = function(str) {
+
+    let split = str.split(' '),
+      a = +split[0],
+      op = split[1], // operation
+      b = +split[2];
+
+    if (!this.methods[op] || isNaN(a) || isNaN(b)) {
+      return NaN;
+    }
+
+    return this.methods[op](a, b);
+  };
+
+  this.addMethod = function(name, func) {
+    this.methods[name] = func;
+  };
+} // 개깔끔
+
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+
+
+
+
+// 이름 매핑하기
+
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let users = [ john, pete, mary ];
+
+// let names = /* 여기에 코드를 작성하세요. */
+// users.map(user => user.name);
+
+
+// alert( names ); // John, Pete, Mary
+
+
+
+// 객체 매핑하기
+
+// let john = { name: "John", surname: "Smith", id: 1 };
+// let pete = { name: "Pete", surname: "Hunt", id: 2 };
+// let mary = { name: "Mary", surname: "Key", id: 3 };
+
+// let users = [ john, pete, mary ];
+
+// let usersMapped = /* 여기에 코드를 작성하세요. */
+
+// users.map(user => ({
+//   fullName: `${user.name} ${user.surname}`,
+//   id: user.id
+// }))
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+// alert( usersMapped[0].id ) // 1
+// alert( usersMapped[0].fullName ) // John Smith
+
+
+
+
+
+
+
+// 나이를 기준으로 객체 정렬하기
+
+// let john = { name: "John", age: 25 };
+// let pete = { name: "Pete", age: 30 };
+// let mary = { name: "Mary", age: 28 };
+
+// let arr = [ pete, john, mary ];
+
+// sortByAge(arr);
+
+// function sortByAge(array) {
+//   array.sort((a, b) => a.age - b.age );
+// }
+
+// // now: [john, mary, pete]
+// alert(arr[0].name); // John
+// alert(arr[1].name); // Mary
+// alert(arr[2].name); // Pete
+
+
+
+
+// 배열 요소 무작위로 섞기 ??? 피셔-예이츠 알고리즘...
+
+// let arr = [1, 2, 3];
+
+
+// shuffle(arr);
+// // arr = [3, 2, 1]
+
+// shuffle(arr);
+// // arr = [2, 1, 3]
+
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+
+
+
+// 평균 나이 구하기
+
+
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+alert( getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
+
+function getAverageAge(array) {
+  return array.reduce((acc, cur) => acc + cur.age, 0) / array.length;
+}
+
+
+// 중복 없는 요소 찾아내기
+
+
+function unique(arr) {
+  /* your code */
+
+  return arr.filter((v,i) => arr.indexOf(v) == i)
+  
+
+}
+
+let strings = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(strings) ); // Hare, Krishna, :-O
